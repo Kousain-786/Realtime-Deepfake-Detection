9076aee7a196e2e27a2add3ec3d076cbccb03ab2
Kousain Ali <sydalihssn786@gmail.com>
2025-05-19 14:50:30 +0500

    added base file and folder structure of the project

---
+++ Backend/RTDFD/RTDeepfake/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RtdeepfakeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'RTDeepfake'
---
+++ Frontend/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+const Signup = () => {
+  return (
+    <Box>
+        <Typography>Hi</Typography>
+    </Box>
+  )
+}
+
+export default Signup
